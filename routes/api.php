536e07b4fd33ec880b73edd399d07209c55c0a55
Samuel Chuks <samydov@gmail.com>
2024-03-18 17:02:07 +0100

    monday

--- routes/api.php
+++ routes/api.php
@@ -103,8 +103,8 @@ Route::prefix('v1')->group(function () {
             Route::post('/admin/investments/payinvestment', [InvestmentController::class, 'payInvestment']);
             Route::post('/admin/investments/justpayinvestment', [InvestmentController::class, 'justpayInvestment']);
             //Route::post('/admin/investments/retrypayment', [InvestmentController::class, 'retrypayment']);
-            Route::post('/admin/investments/payweeklyinvestment', [InvestmentController::class, 'paybulkWeeklyInvestment']);
-            Route::post('/admin/investments/payweeklyfrozeninvestment', [InvestmentController::class, 'paybulkWeeklyFrozenInvestment']);
+            //Route::post('/admin/investments/payweeklyinvestment', [InvestmentController::class, 'paybulkWeeklyInvestment']);
+            //Route::post('/admin/investments/payweeklyfrozeninvestment', [InvestmentController::class, 'paybulkWeeklyFrozenInvestment']);
             //Route::post('/admin/investments/paymonthlyinvestment', [InvestmentController::class, 'paybulkMonthlyInvestment']);
             //Route::post('/admin/investments/paymonthlyfrozeninvestment', [InvestmentController::class, 'paybulkMonthlyFrozenInvestment']);
             //Route::post('/admin/investments/paymonthlyinvestment20th', [InvestmentController::class, 'paybulkMonthlyInvestment2']);

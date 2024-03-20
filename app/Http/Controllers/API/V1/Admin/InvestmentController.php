03eca2ca893d8229982902cd3670043c6b6da5f5
Samuel Chuks <samydov@gmail.com>
2024-03-20 14:54:13 +0100

    monday

--- app/Http/Controllers/API/V1/Admin/InvestmentController.php
+++ app/Http/Controllers/API/V1/Admin/InvestmentController.php
@@ -1199,7 +1199,7 @@ class InvestmentController extends Controller
             array_push($bulkdata, $newdata);
             $k++;
         }
-        print_r($bulkdata); exit();
+        //print_r($bulkdata); exit();
 
         /////////////////
         ///Make Payment
